fa6a5467cbd62c8ca6a7ed6801ddadbfbf69306a
PhilipGough <philip.p.gough@gmail.com>
2016-03-06 18:56:55 +0000

    Added some mongo db connectivity to app

---
+++ config.py
@@ -0,0 +1,8 @@
+WTF_CSRF_ENABLED = False
+SECRET_KEY = 'not_secret'
+
+
+MONGO_HOST = 'ec2-54-194-79-21.eu-west-1.compute.amazonaws.com'
+MONGO_DBNAME = 'test'
+MONGO_USERNAME = 'testu'
+MONGO_PASSWORD = 'testu'
--- testApp/__init__.py
+++ testApp/__init__.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask.ext.pymongo import PyMongo
+
+
+class CreateForm(Form):
+    task = StringField('Task', validators=[DataRequired()])
 
 application = Flask(__name__)
+application.config.from_object('config')
+mongo = PyMongo(application)
 
 
-@application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@application.route("/", methods=['GET', 'POST'])
+def home():
+    results = mongo.db.test.find()
+    tList = []
+    for i in range(results.count()):
+        task = results.next()['Task']
+        tList.append(task)
+    form = CreateForm()
+    if form.validate_on_submit():
+        mongo.db.test.insert_one({'Task': form.task.data})
+        return redirect(url_for('home'))
+    return render_template('index.html', title='Home', form=form, tasks=tList)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
